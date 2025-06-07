f55aed7d5dff467b17671c4c2257b1b1b307a4ac
Avik Das <avikdas567@gmail.com>
2025-06-07 21:28:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import nltk
+nltk.download('punkt')
+nltk.download('punkt_tab')
+
+from nltk.tokenize import word_tokenize
+
+text = "I love learning Natural Language Processing!"
+words = word_tokenize(text)
+st.write(words)
